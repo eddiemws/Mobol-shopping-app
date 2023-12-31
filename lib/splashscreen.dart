c1d45a4a3f05554d9e55e50008be240a43930b8e
eddy <106661971+eddiemws@users.noreply.github.com>
2023-12-31 23:07:09 +0300

    Update splashscreen.dart

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -9,7 +9,16 @@ void main() => runApp(
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Animated Page',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: const AnimatedPage(),
+    );
+  }
+}
 
 class AnimatedPage extends StatefulWidget {
   const AnimatedPage({super.key});
