7345bb58047393b35b231b67f8922cfbfeb8172b
eddy <106661971+eddiemws@users.noreply.github.com>
2024-01-08 23:52:46 +0300

    Update details.dart

--- lib/details.dart
+++ lib/details.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    const MaterialApp(debugShowCheckedModeBanner: false),
+  );
+}
 
+class DetailScreen extends StatelessWidget {
+  final Map<String, String> item;
+
+  const DetailScreen({Key? key, required this.item}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
