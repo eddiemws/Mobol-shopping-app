f70634fa84394edb3903bded32b43fda37a5a41b
eddy <106661971+eddiemws@users.noreply.github.com>
2024-01-16 22:32:53 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,17 @@ void main() {
 }
 
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
 
 
 
